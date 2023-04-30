b4edb82652390bef1ace28968535ab272c15770e
ydai713 <yang.dai@klaviyo.com>
2023-04-30 00:16:14 -0700

    wrap up

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
+"""
+This script generates a mermaid chart of the dependency tree of a dbt model.
+
+It purely uses the model files in the project directory and does not rely on
+the dbt manifest.json file, which means it does not need to run `dbt compile`
+first.
+"""
+import os
 import re
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 from pathlib import Path
+from itertools import chain
+
+
+os.chdir("/home/yang/Code/Klaviyo/company/klaviyo-bi-scripts/src/dbt/")
 
 
 def main():
-    model_name = "tbl_ga_sessions"
-    max_depth = 5
+    # model_name = "tbl_ga_sessions"
+    model_name = "tbl_acquisition_funnel"
+    # model_name = "tbl_kpm_email_base"
+    max_depth = 3
 
     project_root = find_dbt_project_root()
     models_dir = project_root / "models"
+    data_dir = project_root / "data"
+    snapshots_dir = project_root / "snapshots"
 
-    all_models = get_all_models(models_dir)
+    all_models = get_all_models(models_dir, data_dir, snapshots_dir)
 
-    downstream_models = generate_dependency_tree(
+    downstream_models = generate_downstream_tree(
         model_name, all_models, max_depth=max_depth
     )
-    dependency_tree = {
-        model_name: downstream_models
-    }
-    mermaid_chart = dict_to_mermaid_chart(dependency_tree)
-    print(mermaid_chart)
+    downstream_tree = { model_name: downstream_models }
+
+    upstream_models = generate_upstream_tree(
+        model_name, all_models, max_depth=max_depth
+    )
+    upstream_tree = { model_name: upstream_models }
+
+    downstream_chart = dict_to_mermaid_chart(downstream_tree, downstream=True)
+    upstream_chart = dict_to_mermaid_chart(upstream_tree, downstream=False)
+
+    chart = f"""
+        flowchart LR
+        {downstream_chart}
+        {upstream_chart}
+    """
+
+    print(chart)
 
 
 def find_dbt_project_root() -> Path:
@@ -33,61 +61,121 @@ def find_dbt_project_root() -> Path:
     raise FileNotFoundError("Please run the script inside a dbt project.")
 
 
-def get_all_models(models_dir: Path) -> List[Path]:
-    sql_files = list(models_dir.rglob("*.sql"))
-    py_files = list(models_dir.rglob("*.py"))
-    all_files = sql_files + py_files
-    return all_files
+def get_all_models(
+    models_dir: Path,
+    data_dir: Path,
+    snapshots_dir: Path
+) -> Dict[str, str]:
+    """
+    Returns a dictionary of all models in the project.
+    The keys are the model names and the values are the model contents.
+    """
+    all_models = chain(
+        models_dir.rglob("*.sql"), 
+        models_dir.rglob("*.py"),
+        data_dir.rglob("*.csv"),
+        snapshots_dir.rglob("*.sql"),
+    )
+    result = {}
+    for model in all_models: 
+        model_name = model.name.split(".")[0]
+        model_content = model.read_text() if model.suffix == ".sql" else ""
+        result[model_name] = model_content
+    return result
 
 
-def generate_re_pattern(model_name: str):
-    pattern_template = r'\s*ref\(["\']{}["\']\)\s*'
-    pattern_str = pattern_template.format(model_name)
-    pattern = re.compile(pattern_str)
-    return pattern
-
-
-def generate_dependency_tree(
+def is_downstream_model(
     parent_model_name: str,
-    all_models: List[Path],
+    child_model_content: str
+) -> bool:
+    pattern_template = r'\s*ref\(["\']{}["\']\)\s*'
+    pattern_str = pattern_template.format(parent_model_name)
+    pattern = re.compile(pattern_str)
+    return True if pattern.search(child_model_content) else False
+
+
+def extract_model_name_and_source_type(model_content: str) -> Dict[str, str]:
+    """
+    Returns a dictionary of all matched model names and their source types.
+    The keys are the model names and the values are the source types.
+    """
+    pattern = r"s*(source|ref)\s*\(\s*['\"]?(?:[\w-]+',\s*)?['\"]?(.*?)['\"]?\s*\)"
+    matches = re.findall(pattern, model_content)
+
+    if not matches:
+        return {}
+
+    models = {
+        match[1]: match[0]
+        for match in matches
+    }
+
+    return models
+
+
+def generate_downstream_tree(
+    parent_model_name: str,
+    all_models: Dict[str, str],
     current_depth: int = 0,
     max_depth: Optional[int] = None
 ) -> Dict[str, Dict]:
-    dependencies = {}
+    children = {}
 
     if max_depth is not None and current_depth >= max_depth:
-        return dependencies
+        return children
 
-    for model in all_models:
-        model_name = model.name.split(".")[0]
-
-        pattern = generate_re_pattern(parent_model_name)
-
-        f = open(model, "r")
-        content = f.read()
-
-        if pattern.search(content):
-            dependencies[model_name] = generate_dependency_tree(
+    for model_name, model_content in all_models.items():
+        if is_downstream_model(parent_model_name, model_content):
+            children[model_name] = generate_downstream_tree(
                 model_name,
                 all_models,
                 current_depth=current_depth + 1,
                 max_depth=max_depth
             )
-        f.close()
 
-    return dependencies
+    return children
+
+
+def generate_upstream_tree(
+    child_model_name: str,
+    all_models: Dict[str, str],
+    current_depth: int = 0,
+    max_depth: Optional[int] = None
+) -> Dict[str, Dict]:
+    parents = {}
+
+    if max_depth is not None and current_depth >= max_depth:
+        return parents
+
+    # use regular expression to find parten model_name in all_models
+    models = extract_model_name_and_source_type(all_models[child_model_name])
+    for model_name, model_type in models.items():
+        if model_type == "ref":
+            try:
+                parents[model_name] = generate_upstream_tree(
+                    model_name,
+                    all_models,
+                    current_depth=current_depth + 1,
+                    max_depth=max_depth
+                )
+            except KeyError:
+                pass
+        elif model_type == "source":
+            parents[model_name] = {}
+
+    return parents
 
 
 def dict_to_mermaid_chart(
     d: Dict[str, Dict], 
     parent=None,
     mentioned_nodes=None,
-    visited_edges=None
+    visited_edges=None,
+    downstream: bool = False
 ) -> str:
     chart = ""
     
     if parent is None:
-        chart += "flowchart LR\n"
         mentioned_nodes = set()
         visited_edges = set()
     
@@ -96,12 +184,20 @@ def dict_to_mermaid_chart(
             edge = (parent, key)
             if key not in mentioned_nodes:
                 mentioned_nodes.add(key)
-                chart += f"{key}[{key}]\n"
             if edge not in visited_edges:
                 visited_edges.add(edge)
-                chart += f"{parent} --> {key}\n"
+                chart += (
+                    f"{key} --> {parent}\n" if not downstream 
+                    else f"{parent} --> {key}\n"
+                )
         if value:
-            chart += dict_to_mermaid_chart(value, key, mentioned_nodes, visited_edges)
+            chart += dict_to_mermaid_chart(
+                value, 
+                key,
+                mentioned_nodes,
+                visited_edges, 
+                downstream=downstream
+            )
     
     return chart
 
